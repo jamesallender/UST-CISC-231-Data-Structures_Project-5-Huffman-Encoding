63a22be347a1873f1ce3aa05cb2478362dda5d03
James <jamesa52416@gmail.com>
2018-04-24 19:56:59 -0500

    4
    4

--- Compressor.java
+++ Compressor.java
@@ -6,18 +6,18 @@ import javax.swing.JFrame;
 
 public class Compressor {
 	public static void main(String[] args) {
-		//ask user for input file
-		//ask user for output file
-		//read input file
-		//call toString and print it
-		//fill output file with compressed data
+		// ask user for input file
+		// ask user for output file
+		// read input file
+		// call toString and print it
+		// fill output file with compressed data
 	}
-	
+
 	private static void fileReader() {
 		JFrame frame = new JFrame();
 		JFileChooser chooser = new JFileChooser();
-	//	Scanner scanner = null;
-		
+		// Scanner scanner = null;
+
 		frame.setAlwaysOnTop(true);
 		frame.setVisible(false);
 		try {
@@ -25,23 +25,23 @@ public class Compressor {
 				throw new Error("Input file not selected");
 			}
 			String inFile = chooser.getName();
-	//		scanner = new Scanner(file);
-	//		while(file.exists() && scanner.hasNext()) {
-	//			int arrive = scanner.nextInt();
-	//			int duration = scanner.nextInt();
-	//			boolean isWalkIn = scanner.nextBoolean();
-	//			if (forceWalkIns) {
-//					bank.newCustomer(arrive, duration, true);
-//				} else {
-//					bank.newCustomer(arrive, duration, isWalkIn);
-//				}
-//			}
-//		} catch (FileNotFoundException e) {
-//			System.err.println("Data file not found.");
+			// scanner = new Scanner(file);
+			// while(file.exists() && scanner.hasNext()) {
+			// int arrive = scanner.nextInt();
+			// int duration = scanner.nextInt();
+			// boolean isWalkIn = scanner.nextBoolean();
+			// if (forceWalkIns) {
+			// bank.newCustomer(arrive, duration, true);
+			// } else {
+			// bank.newCustomer(arrive, duration, isWalkIn);
+			// }
+			// }
+			// } catch (FileNotFoundException e) {
+			// System.err.println("Data file not found.");
 		} catch (Exception e) {
 			System.err.println("A mysterious error occurred.");
-//			e.printStackTrace(System.err);
+			// e.printStackTrace(System.err);
 		}
-//		scanner.close();
+		// scanner.close();
 	}
 }
